25dbe21bbdba3c3e2417cfc7b5fcec638a30215f
jdy8739 <jdy8739@naver.com>
2025-08-24 14:29:14 +0900

    feat: redirect to login page unless logged in in the server

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
+import { auth } from "@/src/services/firebase";
+import { redirect } from "next/navigation";
+
 const Home = () => {
+  const user = auth.currentUser;
+
+  if (!user) {
+    redirect("/login");
+  }
+
   return <div>Home</div>;
 };
 
