dd530280d500084d245f7f1bf19102f07364ae37
jdy8739 <jdy8739@naver.com>
2025-09-04 21:51:47 +0900

    setting tailwind after build and deploy

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "brand-primary": "#2e4b79ff",
+        "brand-primary-hover": "#1e304eff",
+      },
+    },
+  },
+  plugins: [],
+};
